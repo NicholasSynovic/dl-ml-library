ac24a992dd56432f6ec9738529212f3694ca7793
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-17 13:48:02 -0500

    Test inheritence

--- dl_ml_library/datasets/uci/iris.py
+++ dl_ml_library/datasets/uci/iris.py
@@ -5,13 +5,16 @@ from dl_ml_library.abstract.dataset import Dataset
 
 class Iris(Dataset):
     def __init__(self) -> None:
+        super().__init__()
         self.homeURL: str = "https://archive.ics.uci.edu/dataset/53/iris"
         self.downloadURL: str = "https://archive.ics.uci.edu/static/public/53/iris.zip"
 
     def download(self) -> None:
-        print(self.requestHeaders)
         resp: Response = get(url=self.downloadURL, headers=self.requestHeaders)
         return None
+
+    def load():
+        pass
 
 
 i = Iris()
