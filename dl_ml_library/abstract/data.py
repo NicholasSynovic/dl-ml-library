eff7208d04ba626d49f8c089d85a68ab44a2f76b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-21 13:19:41 -0500

    Add method for generating train, test, and validation splits

--- dl_ml_library/abstract/data.py
+++ dl_ml_library/abstract/data.py
@@ -1,5 +1,32 @@
 from abc import ABC, abstractmethod
 
+from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+
 
 class Data(ABC):
-    pass
+    def __init__(self, df: DataFrame) -> None:
+        self.df: DataFrame = df
+        self.trainingDF: DataFrame = None
+        self.testingDF: DataFrame = None
+        self.validationDF: DataFrame = None
+
+    def trainTestValidationSplit(
+        self,
+        trainSplit: float = 0.7,
+        validationSplit: float = 0.15,
+        testSplit: float = 0.15,
+        seed: int = 42,
+    ) -> None:
+        self.trainingDF, self.testingDF = train_test_split(
+            self.df,
+            test_size=testSplit,
+            random_state=seed,
+            shuffle=True,
+        )
+        self.trainingDF, self.validationDF = train_test_split(
+            self.trainingDF,
+            test_size=validationSplit,
+            random_state=seed,
+            shuffle=True,
+        )
